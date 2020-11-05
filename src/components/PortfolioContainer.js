508364cacc8a341d8a4c19bf7218352639538513
Devin Gillogly <virlym@gmail.com>
2020-11-05 11:23:51 -0800

    base changes

---
+++ src/components/PortfolioContainer.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import NavTabs from "./NavTabs";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+
+class PortfolioContainer extends Component {
+  state = {
+    currentPage: "Home"
+  };
+
+  handlePageChange (page){
+    this.setState({ currentPage: page });
+  };
+
+  render() {
+    return (
+      <div>
+        <NavTabs
+          currentPage={this.state.currentPage}
+          handlePageChange={this.handlePageChange.bind(this)}
+        />
+        {
+          this.state.currentPage === "Blog"
+            ? <Blog />
+            : this.state.currentPage === "Contact"
+              ? <Contact />
+              : this.state.currentPage === "About"
+                ? <About />
+                : <Home />
+        }
+      </div>
+    );
+  }
+}
+
+export default PortfolioContainer;
